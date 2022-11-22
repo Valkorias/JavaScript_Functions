9009f36c0ca2e120a04a6689eafbc6cacd4f6eb8
David Figone <david.louis.figone@gmail.com>
2022-11-22 14:17:23 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -2,6 +2,114 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    for (let i = 1; i <= Math.abs(count); i++) {
+        if (i % 2 != 0) {
+            console.log(i * Math.sign(count));
+        }
+    }
+}
+
+printOdds(-10);
+printOdds(10);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(name, age) {
+    let oldEnoughMsg = `Congrats ${name}! You can drive!`;
+    let tooYoungMsg = `Sorry ${name}, you need to wait ${16 - age} year(s) until you can drive.`;
+
+    if (age < 16) {
+        console.log(tooYoungMsg);
+    } else {
+        console.log(oldEnoughMsg);
+    }
+}
+
+checkAge("David", 21);
+checkAge("James", 15);
+checkAge("Alex", 16);
+
+// Exercise 3 Section
+function checkQuadrant(x, y) {
+    if (x > 0 && y > 0) {
+        return "Quadrant 1";
+    } else if (x < 0 && y > 0) {
+        return "Quadrant 2";
+    } else if (x < 0 && y < 0) {
+        return "Quadrant 3";
+    } else if (x > 0 && y < 0) {
+        return "Quadrant 4";
+    } else if (x == 0 && y != 0) {
+        return "X Axis";
+    } else if (x != 0 && y == 0) {
+        return "Y Axis";
+    } else {
+        return "Origin";
+    }
+}
+
+console.log(checkQuadrant(1, 1));
+console.log(checkQuadrant(-1, 1));
+console.log(checkQuadrant(-1, -1));
+console.log(checkQuadrant(1, -1));
+console.log(checkQuadrant(0, -1));
+console.log(checkQuadrant(1, 0));
+console.log(checkQuadrant(0, 0));
+
+// Exercise 4 Section
+function isValidTriangle(a, b, c) {
+    return a + b > c || a + c > b || b + c > a;
+}
+
+function checkTriangle(a, b, c) {
+    let isValid = isValidTriangle(a, b, c);
+    if (isValid) {
+        if (a == b || b == c) {
+           return `Equilateral`;
+        } else if (a == b || b == c || a == c) {
+            return `Isosceles`
+        } else {
+            return `Scalene`
+        }
+    } else {
+        return `Not a valid triangle.`
+    }
+}
+
+console.log(checkTriangle(2, 3, 4));
+console.log(checkTriangle(2, 2, 2));
+console.log(checkTriangle(1, 2, 2));
+console.log(checkTriangle(1, 1, 2));
+
+// Exercise 5 Section
+
+function dataUsageFeedback(planLimit, day, usage) {
+    let periodLength = 30;
+    let currentAvg = usage / day;
+    let projectedAvg = planLimit / periodLength;
+    let remainingDays = periodLength - day;
+    let remainingData = planLimit - usage;
+    let projectedUsage = remainingDays * currentAvg;
+    let statusMsg; 
+
+    if (currentAvg > projectedAvg) {
+        statusMsg = "EXCEEDING";
+    } else if (currentAvg < prejectedAvg) {
+        statusMsg = "UNDER";
+    } else {
+        statusMsg = "AT";
+    }
+    
+    console.log(`${day} days(s) used, ${remainingDays} day(s) remaining
+    Average prjected use: ${projectedAvg.toFixed(2)} GB/day.
+    You are ${statusMsg} your average daily use (${currentAvg.toFixed(2)} GB/day),
+    continuing this usage, you'll end up using ${
+        planLimit - (usage + projectedUsage)
+    } GB from your data limit.
+    To stay below your data plan, use no more than ${(
+        remainingData / remainingDays
+    ).toFixed(2)} GB/day.`);
+}
+
+dataUsageFeedBack(50, 10, 25);
